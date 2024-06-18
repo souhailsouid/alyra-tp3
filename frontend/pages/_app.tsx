64af41bdb68e8ecaf9c337019419476f38177728
Alex <alexandreborel1994@gmail.com>
2024-06-18 22:18:48 +0200

    feat: Base layout for frontend

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,45 +1,31 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
+import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import type { AppProps } from 'next/app'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-} from 'wagmi/chains';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { arbitrum, base, mainnet, optimism, polygon, sepolia, hardhat } from 'wagmi/chains'
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 
 const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
-  ssr: true,
-});
+    appName: 'RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [hardhat],
+    ssr: true,
+})
 
-const client = new QueryClient();
+const client = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={client}>
+                <RainbowKitProvider>
+                    <Component {...pageProps} />
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    )
 }
 
-export default MyApp;
+export default MyApp
